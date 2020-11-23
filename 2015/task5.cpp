5fce3764925197867a43347c8af5c25f6ef7b485
Mateusz Swiatkowski von Schiller <6883643+matvs@users.noreply.github.com>
2020-11-23 23:46:03 +0100

    Solve riddle of 5th day part2

--- 2015/task5.cpp
+++ 2015/task5.cpp
@@ -40,23 +40,45 @@ bool isStringNice(string input) {
         return true;
     }
 
- return false;
+    return false;
 
 }
+
+bool isStringNice2(string input) {
+    smatch sm;
+    regex overlapped("([a-zA-Z])([a-zA-Z]).*\\1\\2");
+    regex_search(input,sm,overlapped);
+    int numberOfOverlapped = sm.size();
+
+    regex repeatedAndBetween("([a-zA-Z])[a-zA-Z]\\1");
+    regex_search(input,sm,repeatedAndBetween);
+    int numberOfRepeatedAndBetween = sm.size();
+
+    if (numberOfOverlapped && numberOfRepeatedAndBetween) {
+        return true;
+    }
+
+    return false;
+
+}
+
 
 int main(int argc, char* argv[]) {
   std::ifstream infile("task5Input.txt");
   cout << "Doesn't He Have Intern-Elves For This?\n";
     std::string line;
     int numberOfNice = 0;
+    int numberOfNice2 = 0;
       while (std::getline(infile, line)) {
           if (isStringNice(line)) {
               numberOfNice++;
           }
+        if (isStringNice2(line)) {
+              numberOfNice2++;
+          }
     }
-    cout <<numberOfNice;
-
-
+    cout <<numberOfNice << endl;
+    cout <<numberOfNice2 << endl;
    
     return 0;
 }
