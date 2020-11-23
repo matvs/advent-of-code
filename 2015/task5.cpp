cd2d5ba8b591cd952b5630a38251291755accbdf
Mateusz Swiatkowski von Schiller <6883643+matvs@users.noreply.github.com>
2020-11-23 23:36:34 +0100

    Solver task5 using regular expressions like any decent human being would do

--- 2015/task5.cpp
+++ 2015/task5.cpp
@@ -3,9 +3,8 @@
 #include <algorithm>
 
 #include <sstream>
-#include <vector>
-#include <map>
 #include <iterator>
+#include <regex>
 
 #include <fstream>
 
@@ -13,87 +12,35 @@ using namespace std;
 
 
 bool isStringNice(string input) {
-    vector<string> required = {
-        "a",
-        "e",
-        "i",
-        "o",
-        "u",
-        };
-    int minOccuranceOfRequired = 3;
-    int requiredOcuurances = 0;
+    const int minNumberOfVowels = 3;
+    smatch sm;
+   //required
+    int numberOfFoundVowels = 0;
 
-    for (vector<string>::iterator it = required.begin(); it != required.end(); ++it) {
-        size_t found = -1;
-        // cout << *it << endl;
-        do {
-           found = input.find(*it, found == -1 ? 0 : found + 1);
-            //    cout << found << endl << input << endl;
-            if (found != std::string::npos) {
-                requiredOcuurances++;
-            }
-               if (requiredOcuurances >= minOccuranceOfRequired) {
-           break;
-       }
-        } while (found!=std::string::npos);
-       if (requiredOcuurances >= minOccuranceOfRequired) {
-           break;
-       }
-        
+    regex vowels("[aeiou]");
+    sregex_iterator iter(input.begin(), input.end(), vowels);
+    sregex_iterator end;
+
+    while(iter != end)
+    {
+       ++numberOfFoundVowels;
+       iter++;
     }
 
-    string twoChars = "";
-    int twoInRow = 0;
-        for (string::iterator it = input.begin(); it != input.end(); ++it) {
-            if (twoChars.length() == 2) {
-                // cout << twoChars <<  " ";
-                if (twoChars[0] == twoChars[1]) {
-                    twoInRow++;
-                    break;
-                }
-                twoChars = "";
-                it--;
-            }
+    regex doubled("([a-zA-Z])\\1");
+    regex_search(input,sm,doubled);
+    int numberOfDoubled = sm.size();
 
-            twoChars += *it;
-        
+    //forbidden
+    regex forbidden("ab|cd|pq|xy");
+    regex_search(input,sm,forbidden);
+    if (sm.size() > 0) {
+        return false;
+    } else if(numberOfFoundVowels >= minNumberOfVowels && numberOfDoubled > 0) {
+        return true;
     }
 
-     if (twoChars.length() == 2) {
-                // cout << twoChars <<  " ";
-                if (twoChars[0] == twoChars[1]) {
-                    twoInRow++;
-               
-                }
-          
-            }
-
-        vector<string> forbidden = {
-        "ab",
-        "cd",
-        "pq",
-        "xy",
-        };
-
-         int maxOccuranceOfForbidden = 1;
-
-             for (vector<string>::iterator it = forbidden.begin(); it != forbidden.end(); ++it) {
-        size_t found = -1;
-   
-           found = input.find(*it, found == -1 ? 0 : found + 1);
-            if (found != std::string::npos) {
-                return false;
-            }
-         
-        
-    }
-        //  cout << requiredOcuurances << minOccuranceOfRequired << twoInRow << endl;
-       if (requiredOcuurances >= minOccuranceOfRequired && twoInRow >=1) {
-           return true;
-       }
-
-       return false;
- 
+ return false;
 
 }
 
@@ -107,8 +54,11 @@ int main(int argc, char* argv[]) {
               numberOfNice++;
           }
     }
-    // cout << isStringNice("arehecc") << endl;;
     cout <<numberOfNice;
+
+
+   
+    return 0;
 }
 
 
