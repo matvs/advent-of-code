32477293ee645bd59c1cb80851f752a98af8b174
matvs <6883643+matvs@users.noreply.github.com>
2023-12-30 13:28:48 +0100

    more solutions

--- 2023/day20.py
+++ 2023/day20.py
@@ -1,5 +1,6 @@
 import re
 from enum import Enum
+from queue import Queue
 
 class State(Enum):
     LOW = "low"
@@ -19,6 +20,8 @@ def parseInput(lines):
 
 
 allParts = {}
+def assignOutputParts():
+    pass
 def assignConjuctionsInputs():
     for label,part in allParts.items():
     #print(label, part)
@@ -34,6 +37,9 @@ def initAllParts(flipflops, conjunctions):
     for conjunction in conjunctions:
         allParts[conjunction[0]] = Conjunction([], conjunction[1].split(','),conjunction[0])
         
+    allParts['output'] = Part([],[], 'ouput')
+    allParts['rx'] = Part([],[], 'rx')
+        
     assignConjuctionsInputs()
 
 
@@ -48,6 +54,20 @@ class Part:
         self.inputs = [input.strip() for input in inputs]
         self.outputs = [output.strip() for output in outputs]
         self.label = label
+        self.state = None
+        
+    def propagate(self,pulse, input = None):
+        transitions = []
+        for output in self.outputs:
+            def receive(output):
+                return lambda: allParts[output].receive(pulse, self.label)
+            #print(f'{self.label} -{pulse}-> {output}')
+            count(pulse)
+            transitions.append(receive(output))
+        return transitions
+    
+    def receive(self, pulse, input):
+        return self.propagate(self.state, input)
 
 class FlipFlop(Part):
     def __init__(self,inputs, outputs, label):
@@ -59,15 +79,17 @@ class FlipFlop(Part):
             self.state = flip(self.state)
 
     def receive(self,pulse, input = None):
+        print(f'{input} -{pulse}-> {self.label}')
         if pulse == State.LOW:
-            for output in self.outputs:
-                allParts[output].updateState(pulse, self.label)
+            self.updateState(pulse)
+            
+            return self.propagate(self.state, input)
+        return []
           
-            for output in self.outputs:
-                if output in allParts:
-                    count(self.state)
-                    print(self.label,' -', self.state,' ', output)
-                    allParts[output].receive(self.state, self.label)
+           
+                    
+    def __repr__(self) -> str:
+        return f'FlipFlop {self.state}'
 
 class Conjunction(Part):
     def __init__(self,inputs, outputs, label):
@@ -78,32 +100,29 @@ class Conjunction(Part):
     def updateState(self, pulse, input = None):
         self.inputs[input] = pulse
         self.state = State.LOW
-        for key,value in allParts.items():
+        for key,value in self.inputs.items():
             if value == State.LOW:
                 self.state = State.HIGH
                 break
 
 
     def receive(self,pulse,input = None):
-        for output in self.outputs:
-            allParts[output].updateState(pulse, self.label)
-        for output in self.outputs:
-            if output in allParts:
-                count(self.state)
-                print(self.label,' -', self.state,' ', output)
-                allParts[output].receive(self.state, self.label)
+        print(f'{input} -{pulse}-> {self.label}')
+        self.updateState(pulse, input)
+        
+        return self.propagate(self.state, input)
+                
+    def __repr__(self) -> str:
+        return f'Conjunction {self.state}'
 
 class Broadcast(Part):
     def __init__(self,inputs, outputs, label):
         super().__init__(inputs, outputs, label)
 
-    def receive(self,pulse, input = None):
-        for output in self.outputs:
-             allParts[output].updateState(pulse, self.label)
-        for output in self.outputs:
-            count(pulse)
-            print(self.label,' -', pulse,' ', output)
-            allParts[output].receive(pulse, self.label)
+
+
+            
+        
 
 
 
@@ -115,8 +134,16 @@ with open('/home/matvs/Projects/advent-of-code/2023/day20.input.txt') as f:
     broadcaster = Broadcast([], broadcasters[0].split(','), 'broadcaster')
     
     initAllParts(flipflops, conjunctions)
- 
-    broadcaster.receive(State.LOW)
-    count(State.LOW)
+    
+    for i in range(1000):
+        transitions = Queue()
+        transitions.put(lambda: broadcaster.propagate(State.LOW))
+        count(State.LOW)
+        
+        while not transitions.empty():
+            currentTransition = transitions.get()
+            nextTransitions = currentTransition()
+            for trans in nextTransitions:
+                transitions.put(trans)
 
-    print(counts)
+    print(counts[State.HIGH] * counts[State.LOW])
