cd8973bd4d1d9b56be7e18cb878804dc081a8692
matvs <6883643+matvs@users.noreply.github.com>
2024-01-08 18:07:12 +0100

    more solutions

--- 2023/day17.py
+++ 2023/day17.py
@@ -5,83 +5,6 @@ import time
 import os
 import heapq
 clear = lambda: os.system('clear')
-
-def getMovementDirection(node, nextNode):
-    return (nextNode.x - node.x, nextNode.y - node.y)
-
-def dijkstra(graph, start, end):
-    # Initialize distances and priority queue
-    distances = {node: float('infinity') for node in graph}
-    distances[start] = 0
-    priority_queue = [(0, start, (0,0), 0)]
-
-    while priority_queue:
-        current_distance, current_node, lastMove, moveCount = heapq.heappop(priority_queue)
-        
-        #if current_node == end:
-           # return distances[current_node]  
-
-        # Only proceed if we have found a shorter way to current_node
-        if current_distance > distances[current_node]:
-            continue
-
-        for neighbor in current_node.neighbors:
-            if neighbor.isVisited:
-                continue
-            
-            move = getMovementDirection(current_node, neighbor)
-            consequentMoveCount = moveCount + 1 if move == lastMove else 1
-            
-            if consequentMoveCount > 3:
-                continue
-            
-            distance = current_distance + neighbor.value
-     
-            # If a shorter path is found
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
-                heapq.heappush(priority_queue, (distance, neighbor, move, consequentMoveCount))
-        
-        current_node.isVisited = True
-    
-    return distances
-
-
-class Node:
-    def __init__(self, value,x,y) -> None:
-        self.value = int(value)
-        self.isVisited = False
-        self.neighbors = []
-        self.x = x
-        self.y = y
-        
-    def __str__(self) -> str:
-        return str(self.value)
-        
-    def __repr__(self) -> str:
-        return str(self.value)
-    
-    def __eq__(self, other: object) -> bool:
-        if other == None:
-            return False
-        return self.value == other.value
-    
-    def __lt__(self, other: object) -> bool:
-        return self.value < other.value
-    
-    def __hash__(self) -> int:
-        return hash(str(self.value) + str(self.x) + str(self.y))
-            
-
-def addNeighbours(nodes):
-    for y in range(len(nodes)):
-        for x in range(len(nodes[y])):
-            directions = [(1,0),(-1,0),(0,1),(0,-1)]
-            for dir in directions:
-                newX = x + dir[0]
-                newY = y + dir[1]
-                if newX >= 0 and newX < len(nodes[y]) and newY >= 0 and newY < len(nodes):
-                    nodes[y][x].neighbors.append(nodes[newY][newX])
                     
 def parseInput(file):
     lines = file.readlines()
@@ -89,23 +12,14 @@ def parseInput(file):
     
     return lines
 
-def mapToNodes(lines):
-    nodes = [[Node(field,x,y) for x,field in enumerate(row)] for y,row in enumerate(lines)]
-    addNeighbours(nodes)
-    # flatten nodes
-    nodes = [node for sublist in nodes for node in sublist]  
-    return nodes 
-
-
-def find_shortest_path(grid, start, end):
+def dijkstra(grid, start, end):
     rows, cols = len(grid), len(grid[0])
-    directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]  # Right, Down, Up, Left
-    dir_names = ["R", "D", "U", "L"]
+    directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 
     # Check if a move is valid
     def is_valid(x, y):
-        return 0 <= x < rows and 0 <= y < cols and grid[x][y] != -1
-
+        return 0 <= x < rows and 0 <= y < cols
+    
     # State: (total_cost, x, y, last_direction, consecutive_moves)
     start_state = (0, start[0], start[1], -1, 0)
     visited = set()
@@ -123,6 +37,7 @@ def find_shortest_path(grid, start, end):
 
             if is_valid(nx, ny) and (nx, ny) not in visited:
                 new_cost = cost + grid[nx][ny]
+                #print(nx,ny, new_cost)
                 new_consec_moves = consec_moves + 1 if i == last_dir else 1
 
                 # Check direction constraint
@@ -132,17 +47,14 @@ def find_shortest_path(grid, start, end):
 
         visited.add((x, y))
 
-    return -1  # Path not found          
+    return -1  # Path not found 
+         
 if __name__ == '__main__':
     with open('/home/matvs/Projects/advent-of-code/2023/day17.input.txt') as f:
         lines = parseInput(f)
-        nodes = mapToNodes(lines)
         
-        grid = [[int(field) for x,field in enumerate(row)] for y,row in enumerate(lines)]
-        print(grid)
-        
-        print(find_shortest_path(grid, (0,0), (len(grid)-1, len(grid[0])-1)))
-        print(dijkstra(nodes, nodes[0], nodes[-1]))
+        grid = [[int(field) for field in row] for row in lines]
+        print(dijkstra(grid, (0,0), (len(grid)-1, len(grid[0])-1)))
         
                 
 
